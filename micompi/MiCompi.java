cc326a98a2f9c5899cc1c97efe35f522f1c4854e
Carlos Eduardo <cerivera2002@gmail.com>
2023-03-30 11:13:51 -0700

    Compilador terminado el analisis lexico pero, al abrir comentario no realiza el error del cierrede este mismo

--- micompi/MiCompi.java
+++ micompi/MiCompi.java
@@ -1,12 +1,13 @@
+package micompi;
+
 import java.io.IOException;
 
 public class MiCompi {
     nodo p;
-    static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         lexico lexico = new lexico();
         if (!lexico.errorEncontrado) {
             System.out.println("Analisis lexico terminado");
-            
-        }
+        }       
     }
 }
--- micompi/nodo.java
+++ micompi/nodo.java
@@ -1,4 +1,4 @@
-
+package micompi;
 
 class nodo {
     String lexema;
